5a354455133ace91739d6b61135f457054e6af61
Favyen Bastani <fbastani@perennate.com>
2023-05-01 01:09:22 +0000

    Move logic from map to a generic gameplay scene

--- Gameplay.gd
+++ Gameplay.gd
@@ -1,15 +1,19 @@
-extends TileMap
+extends Node2D
 
-var open_tile_id = 3
-var wall_tile_id = 4
+@export var map_scene: PackedScene
 
-var cell_size = 128
+const open_tile_id = 3
+const wall_tile_id = 4
+const cell_size = 128
 
 var player
+var tile_map
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	player = get_node('Player')
+	tile_map = map_scene.instantiate()
+	add_child(tile_map)
+	player = tile_map.get_node('Player')
 	set_player_position(Vector2i(0, 0))
 
 func get_player_position():
@@ -24,16 +28,16 @@ func _process(delta):
 	var proposed_action = false
 	var target: Vector2i
 	
-	if Input.is_action_just_released("move_left"):
+	if Input.is_action_just_pressed("move_left"):
 		target = Vector2i(-1, 0)
 		proposed_action = true
-	elif Input.is_action_just_released("move_right"):
+	elif Input.is_action_just_pressed("move_right"):
 		target = Vector2i(1, 0)
 		proposed_action = true
-	elif Input.is_action_just_released("move_up"):
+	elif Input.is_action_just_pressed("move_up"):
 		target = Vector2i(0, -1)
 		proposed_action = true
-	elif Input.is_action_just_released("move_down"):
+	elif Input.is_action_just_pressed("move_down"):
 		target = Vector2i(0, 1)
 		proposed_action = true
 	
@@ -41,7 +45,7 @@ func _process(delta):
 		return
 	
 	var dst = get_player_position() + target
-	var dst_tile_id = get_cell_source_id(0, dst)
+	var dst_tile_id = tile_map.get_cell_source_id(0, dst)
 	if dst_tile_id == wall_tile_id:
 		return
 	
