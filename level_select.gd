582361a4f85a8244d5afaee20b674b6a356e602d
Josh Cai <yuanshi419@gmail.com>
2023-05-19 16:45:36 -0700

    Add instructions to menu text, connect level 4 to level select

--- level_select.gd
+++ level_select.gd
@@ -28,3 +28,7 @@ func _on_level_2_button_pressed():
 
 func _on_level_3_button_pressed():
     get_tree().change_scene_to_file("res://levels/map_3.tscn")
+
+
+func _on_level_4_button_pressed():
+    get_tree().change_scene_to_file("res://levels/map_3.tscn")
